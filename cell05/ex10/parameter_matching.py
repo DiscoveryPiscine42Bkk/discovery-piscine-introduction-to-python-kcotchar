e3c7d0c6b11143d8b9126722bde826212ec5d2aa
kcotchar <68030029@kmitl.ac.th>
2025-06-18 22:23:17 +0000

    5.2

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys #ต้องrunในbashผลถึงจะออกตามโจทย์
+
+if len(sys.argv) == 2:
+    parameter = sys.argv[1]
+    user_input = input ("what was the parameter? ")
+
+    if user_input == parameter:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+else:
+   print("none")
